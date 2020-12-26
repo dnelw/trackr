504cef7aeb4c2957d864e7e1e2a3d807d46401b3
Daniel Wang <accounts@dnelw.com>
2020-12-25 19:46:55 -0500

    Setup snowpack and create-react-app

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import logo from "../assets/logo.svg";
+import "../styles/App.css";
+
+function App() {
+  // Create the count state.
+  const [count, setCount] = useState(0);
+  // Create the counter (+1 every second).
+  useEffect(() => {
+    const timer = setTimeout(() => setCount(count + 1), 1000);
+    return () => clearTimeout(timer);
+  }, [count, setCount]);
+  // Return the App component.
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Page has been open for <code>{count}</code> seconds.
+        </p>
+        <p>Env variable: {`${import.meta.env.SNOWPACK_PUBLIC_TEST_VAR}`}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
