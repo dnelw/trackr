df32d4de8bd1a4b546ed607bac3cb8915ec66529
Daniel Wang <accounts@dnelw.com>
2020-12-25 21:15:35 -0500

    Docker for backend and docker compose

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-const port = 4000;
+const port = process.env.TRACKR_API_PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,6 +12,6 @@ app.use(bodyParser.json());
 const helloworldRouter = require("./routes/helloworld");
 app.use("/", helloworldRouter);
 
-app.listen(4000, () => {
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
