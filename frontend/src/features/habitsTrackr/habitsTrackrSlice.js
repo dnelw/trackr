682e3a8f7633a391e7fd73b02fdf0bba123a96c4
Daniel Wang <accounts@dnelw.com>
2021-01-06 18:56:06 +0000

    Create habits reducer and slice

---
+++ frontend/src/features/habitsTrackr/habitsTrackrSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const habitsTrackrSlice = createSlice({
+  name: "habitsTrackr",
+  initialState: {},
+  reducers: {},
+});
+
+export const {} = habitsTrackrSlice.actions;
+
+export default habitsTrackrSlice.reducer;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import weightTrackrReducer from "./features/weightTrackr/weightTrackrSlice";
+import habitsTrackrReducer from "./features/habitsTrackr/habitsTrackrSlice";
 
 export default configureStore({
   reducer: {
     weightTrackr: weightTrackrReducer,
+    habitsTrackr: habitsTrackrReducer,
   },
 });
